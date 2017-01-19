75565643fe9eda0fcec1337a2745ec8abcf715b1
Edson Rodrigues <edsonrodsilva@gmail.com>
2017-01-19 00:58:09 -0200

    First Commit

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Columbia\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'Columbia\Model' => 'Columbia\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        //
+    }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Projeto modelo
+
+Projeto modelo para criacao de novos projetos.
+
+Contem
+- Laravel 5.3
+- Laravel Collective
+- Laravel 5 Repositories
+
+The Laravel framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
